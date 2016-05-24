21c96c953376f5913158c47bd5a0e2a14f596385
foryou929 <zxcvwert3458@gmail.com>
2016-05-24 22:25:28 +0000

    Add component tests

---
+++ test/google_test.js
@@ -0,0 +1,139 @@
+import {renderComponent, expect} from './test_helper';
+import GoogleLogin from '../src/google';
+
+describe('Google Login', () => {
+    let component;
+    let propsObj;
+    let defaultText = 'Login with Google';
+
+    describe('With default props', () => {
+
+        beforeEach(() => {
+            propsObj = {
+                callback(response) {},
+                clientId: '658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'
+            };
+            component = renderComponent(GoogleLogin, propsObj);
+        });
+        
+        it('shows the button', () => {
+            expect(component).to.exist;
+        });
+
+        it('displays correct button text', () => {
+            expect(component).to.have.text(defaultText);
+        });
+
+        it('does not have a class attr', () => {
+            expect(component).to.not.have.attr('class');
+        });
+        
+        it('has inline styles', () => {
+            expect(component).to.have.attr('style');
+        });
+        
+    });
+
+    describe('With custom text and default props', () => {
+        
+        const buttonText ='buttonText'; 
+        
+        beforeEach(() => {
+            propsObj = {
+                callback(response) {},
+                clientId: '658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com',
+                buttonText
+            };
+            component = renderComponent(GoogleLogin, propsObj);
+        });
+
+        it('shows the button', () => {
+            expect(component).to.exist;
+        });
+        
+        it('displays correct button text', () => {
+            expect(component).to.have.text(buttonText);
+        });
+
+        it('does not have a class attr', () => {
+            expect(component).to.not.have.attr('class');
+        });
+        
+        it('has inline styles', () => {
+            expect(component).to.have.attr('style');
+        });
+
+    });
+
+    describe('With custom class and default props', () => {
+        
+        let cssClass ='test-class'; 
+        
+        beforeEach(() => {
+            propsObj = {
+                callback(response) {},
+                clientId: '658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com',
+                cssClass
+            };
+            component = renderComponent(GoogleLogin, propsObj);
+        });
+
+        it('shows the button', () => {
+            expect(component).to.exist;
+        });
+        
+        it('displays correct button text', () => {
+            expect(component).to.have.text(defaultText);
+        });
+
+        it('has a class attr with custom class', () => {
+            expect(component).to.have.attr('class', cssClass);
+        });
+        
+        it('does not have inline styles', () => {
+            expect(component).to.not.have.attr('style');
+        });
+        
+    });
+
+    describe('With children, custom text, and default props', () => {
+        
+        const children = 'test';
+        const buttonText ='buttonText'; 
+
+        beforeEach(() => {
+            propsObj = {
+                callback(response) {},
+                clientId: '658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com',
+                buttonText
+            };
+            component = renderComponent(GoogleLogin, propsObj, children);
+        });
+        
+        it('shows the button', () => {
+            expect(component).to.exist;
+        });
+
+        it('displays children text', () => {
+            expect(component).to.have.text(children);
+        });
+        
+        it('does not display default text', () => {
+            expect(component).to.not.have.text(defaultText);
+        });
+        
+        it('does not display custom text', () => {
+            expect(component).to.not.have.text(buttonText);
+        });
+        
+        it('does not have a class attr', () => {
+            expect(component).to.not.have.attr('class');
+        });
+        
+        it('has inline styles', () => {
+            expect(component).to.have.attr('style');
+        });
+        
+    });
+
+});
