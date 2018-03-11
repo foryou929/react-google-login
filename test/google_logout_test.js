2fe289f981f0f83409512a65f5db0b5d0f34ebe4
foryou929 <zxcvwert3458@gmail.com>
2018-03-11 17:50:12 -0500

    update tests to cover dist build

--- test/google_logout_test.js
+++ test/google_logout_test.js
@@ -1,196 +1,383 @@
-import { renderComponent, expect } from './test_helper';
-import { GoogleLogout } from '../src/index';
+import { renderComponent, expect } from './test_helper'
+import { GoogleLogout } from '../src/index'
+import { GoogleLogout as GoogleLogoutDist } from '../dist/google-login'
 
 describe('Google Logout', () => {
-    const defaultText = 'Logout';
-    let component;
-    let propsObj;
+  const defaultText = 'Logout'
+  let component
+  let propsObj
 
-    describe('With default props', () => {
-        beforeEach(() => {
-            propsObj = {
-            };
-            component = renderComponent(GoogleLogout, propsObj);
-        });
+  describe('With default props', () => {
+    beforeEach(() => {
+      propsObj = {}
+      component = renderComponent(GoogleLogout, propsObj)
+    })
 
-        it('shows the button', () => {
-            expect(component).to.exist;
-        });
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
 
-        it('displays correct button text', () => {
-            expect(component).to.have.text(defaultText);
-        });
+    it('displays correct button text', () => {
+      expect(component).to.have.text(defaultText)
+    })
 
-        it('does not have a class attr', () => {
-            expect(component).to.not.have.attr('class');
-        });
+    it('does not have a class attr', () => {
+      expect(component).to.not.have.attr('class')
+    })
 
-        it('has inline styles', () => {
-            expect(component).to.have.attr('style');
-        });
+    it('has inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
 
-        it('displays a button element when tag prop is not set', () => {
-            expect(component.get(0).tagName).to.equal('BUTTON');
-        });
-    });
-    describe('With custom text and default props', () => {
-        const buttonText = 'buttonText';
+    it('displays a button element when tag prop is not set', () => {
+      expect(component.get(0).tagName).to.equal('BUTTON')
+    })
+  })
+  describe('With custom text and default props', () => {
+    const buttonText = 'buttonText'
 
-        beforeEach(() => {
-            propsObj = {
-                buttonText
-            };
-            component = renderComponent(GoogleLogout, propsObj);
-        });
+    beforeEach(() => {
+      propsObj = {
+        buttonText
+      }
+      component = renderComponent(GoogleLogout, propsObj)
+    })
 
-        it('shows the button', () => {
-            expect(component).to.exist;
-        });
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
 
-        it('displays correct button text', () => {
-            expect(component).to.have.text(buttonText);
-        });
+    it('displays correct button text', () => {
+      expect(component).to.have.text(buttonText)
+    })
 
-        it('does not have a class attr', () => {
-            expect(component).to.not.have.attr('class');
-        });
+    it('does not have a class attr', () => {
+      expect(component).to.not.have.attr('class')
+    })
 
-        it('has inline styles', () => {
-            expect(component).to.have.attr('style');
-        });
+    it('has inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
+  })
+  describe('With custom class and default props', () => {
+    let className = 'test-class'
 
-    });
-    describe('With custom class and default props', () => {
-        let className = 'test-class';
+    beforeEach(() => {
+      propsObj = {
+        className
+      }
+      component = renderComponent(GoogleLogout, propsObj)
+    })
 
-        beforeEach(() => {
-            propsObj = {
-                className
-            };
-            component = renderComponent(GoogleLogout, propsObj);
-        });
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
 
-        it('shows the button', () => {
-            expect(component).to.exist;
-        });
+    it('displays correct button text', () => {
+      expect(component).to.have.text(defaultText)
+    })
 
-        it('displays correct button text', () => {
-            expect(component).to.have.text(defaultText);
-        });
+    it('has a class attr with custom class', () => {
+      expect(component).to.have.attr('class', className)
+    })
 
-        it('has a class attr with custom class', () => {
-            expect(component).to.have.attr('class', className);
-        });
+    it('does not have inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
+  })
+  describe('With custom class and custom style', () => {
+    let className = 'test-class'
+    let style = { color: 'red' }
 
-        it('does not have inline styles', () => {
-            expect(component).to.have.attr('style');
-        });
+    beforeEach(() => {
+      propsObj = {
+        className,
+        style
+      }
+      component = renderComponent(GoogleLogout, propsObj)
+    })
 
-    });
-    describe('With custom class and custom style', () => {
-        let className = 'test-class';
-        let style = { color: 'red' };
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
 
-        beforeEach(() => {
-            propsObj = {
-                className,
-                style,
-            };
-            component = renderComponent(GoogleLogout, propsObj);
-        });
+    it('displays correct button text', () => {
+      expect(component).to.have.text(defaultText)
+    })
 
-        it('shows the button', () => {
-            expect(component).to.exist;
-        });
+    it('has a class attr with custom class', () => {
+      expect(component).to.have.attr('class', className)
+    })
 
-        it('displays correct button text', () => {
-            expect(component).to.have.text(defaultText);
-        });
+    it('to have custom inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
+  })
+  describe('With children, custom text, and default props', () => {
+    const children = 'test'
+    const buttonText = 'buttonText'
 
-        it('has a class attr with custom class', () => {
-            expect(component).to.have.attr('class', className);
-        });
+    beforeEach(() => {
+      propsObj = {
+        children,
+        buttonText
+      }
+      component = renderComponent(GoogleLogout, propsObj)
+    })
 
-        it('to have custom inline styles', () => {
-            expect(component).to.have.attr('style');
-        });
-    });
-    describe('With children, custom text, and default props', () => {
-        const children = 'test';
-        const buttonText = 'buttonText';
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
 
-        beforeEach(() => {
-            propsObj = {
-                children,
-                buttonText,
-            };
-            component = renderComponent(GoogleLogout, propsObj);
-        });
+    it('displays children text', () => {
+      expect(component).to.have.text(children)
+    })
 
-        it('shows the button', () => {
-            expect(component).to.exist;
-        });
+    it('does not display default text', () => {
+      expect(component).to.not.have.text(defaultText)
+    })
 
-        it('displays children text', () => {
-            expect(component).to.have.text(children);
-        });
+    it('does not display custom text', () => {
+      expect(component).to.not.have.text(buttonText)
+    })
 
-        it('does not display default text', () => {
-            expect(component).to.not.have.text(defaultText);
-        });
+    it('does not have a class attr', () => {
+      expect(component).to.not.have.attr('class')
+    })
 
-        it('does not display custom text', () => {
-            expect(component).to.not.have.text(buttonText);
-        });
+    it('has inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
+  })
+  describe('With style and default props', () => {
+    let style = {
+      padding: '10px',
+      fontSize: '20px'
+    }
 
-        it('does not have a class attr', () => {
-            expect(component).to.not.have.attr('class');
-        });
+    beforeEach(() => {
+      propsObj = {
+        style
+      }
+      component = renderComponent(GoogleLogout, propsObj)
+    })
 
-        it('has inline styles', () => {
-            expect(component).to.have.attr('style');
-        });
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
 
-    });
-    describe('With style and default props', () => {
-        let style = {
-            'padding': '10px',
-            'fontSize': '20px'
-        };
+    it('displays correct button text', () => {
+      expect(component).to.have.text(defaultText)
+    })
 
-        beforeEach(() => {
-            propsObj = {
-                style
-            };
-            component = renderComponent(GoogleLogout, propsObj);
-        });
+    it('has a style with custom class', () => {
+      expect(component).to.have.attr('style', 'padding: 10px; font-size: 20px; opacity: 0.6;')
+    })
+  })
+  describe('With handles custom tag prop', () => {
+    beforeEach(() => {
+      propsObj = {
+        tag: 'div'
+      }
+      component = renderComponent(GoogleLogout, propsObj)
+    })
 
-        it('shows the button', () => {
-            expect(component).to.exist;
-        });
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
 
-        it('displays correct button text', () => {
-            expect(component).to.have.text(defaultText);
-        });
+    it('displays a div element when tag prop is set to div', () => {
+      expect(component.get(0).tagName).to.equal('DIV')
+    })
+  })
+})
 
-        it('has a style with custom class', () => {
-            expect(component).to.have.attr('style', 'padding: 10px; font-size: 20px; opacity: 0.6;');
-        });
-    });
-    describe('With handles custom tag prop', () => {
-        beforeEach(() => {
-            propsObj = {
-                tag: 'div',
-            };
-            component = renderComponent(GoogleLogout, propsObj);
-        });
+describe('Google Logout Dist', () => {
+  const defaultText = 'Logout'
+  let component
+  let propsObj
 
-        it('shows the button', () => {
-            expect(component).to.exist;
-        });
+  describe('With default props', () => {
+    beforeEach(() => {
+      propsObj = {}
+      component = renderComponent(GoogleLogoutDist, propsObj)
+    })
 
-        it('displays a div element when tag prop is set to div', () => {
-            expect(component.get(0).tagName).to.equal('DIV');
-        });
-    });
-});
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
+
+    it('displays correct button text', () => {
+      expect(component).to.have.text(defaultText)
+    })
+
+    it('does not have a class attr', () => {
+      expect(component).to.not.have.attr('class')
+    })
+
+    it('has inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
+
+    it('displays a button element when tag prop is not set', () => {
+      expect(component.get(0).tagName).to.equal('BUTTON')
+    })
+  })
+  describe('With custom text and default props', () => {
+    const buttonText = 'buttonText'
+
+    beforeEach(() => {
+      propsObj = {
+        buttonText
+      }
+      component = renderComponent(GoogleLogoutDist, propsObj)
+    })
+
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
+
+    it('displays correct button text', () => {
+      expect(component).to.have.text(buttonText)
+    })
+
+    it('does not have a class attr', () => {
+      expect(component).to.not.have.attr('class')
+    })
+
+    it('has inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
+  })
+  describe('With custom class and default props', () => {
+    let className = 'test-class'
+
+    beforeEach(() => {
+      propsObj = {
+        className
+      }
+      component = renderComponent(GoogleLogoutDist, propsObj)
+    })
+
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
+
+    it('displays correct button text', () => {
+      expect(component).to.have.text(defaultText)
+    })
+
+    it('has a class attr with custom class', () => {
+      expect(component).to.have.attr('class', className)
+    })
+
+    it('does not have inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
+  })
+  describe('With custom class and custom style', () => {
+    let className = 'test-class'
+    let style = { color: 'red' }
+
+    beforeEach(() => {
+      propsObj = {
+        className,
+        style
+      }
+      component = renderComponent(GoogleLogoutDist, propsObj)
+    })
+
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
+
+    it('displays correct button text', () => {
+      expect(component).to.have.text(defaultText)
+    })
+
+    it('has a class attr with custom class', () => {
+      expect(component).to.have.attr('class', className)
+    })
+
+    it('to have custom inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
+  })
+  describe('With children, custom text, and default props', () => {
+    const children = 'test'
+    const buttonText = 'buttonText'
+
+    beforeEach(() => {
+      propsObj = {
+        children,
+        buttonText
+      }
+      component = renderComponent(GoogleLogoutDist, propsObj)
+    })
+
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
+
+    it('displays children text', () => {
+      expect(component).to.have.text(children)
+    })
+
+    it('does not display default text', () => {
+      expect(component).to.not.have.text(defaultText)
+    })
+
+    it('does not display custom text', () => {
+      expect(component).to.not.have.text(buttonText)
+    })
+
+    it('does not have a class attr', () => {
+      expect(component).to.not.have.attr('class')
+    })
+
+    it('has inline styles', () => {
+      expect(component).to.have.attr('style')
+    })
+  })
+  describe('With style and default props', () => {
+    let style = {
+      padding: '10px',
+      fontSize: '20px'
+    }
+
+    beforeEach(() => {
+      propsObj = {
+        style
+      }
+      component = renderComponent(GoogleLogoutDist, propsObj)
+    })
+
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
+
+    it('displays correct button text', () => {
+      expect(component).to.have.text(defaultText)
+    })
+
+    it('has a style with custom class', () => {
+      expect(component).to.have.attr('style', 'padding: 10px; font-size: 20px; opacity: 0.6;')
+    })
+  })
+  describe('With handles custom tag prop', () => {
+    beforeEach(() => {
+      propsObj = {
+        tag: 'div'
+      }
+      component = renderComponent(GoogleLogoutDist, propsObj)
+    })
+
+    it('shows the button', () => {
+      expect(component).to.exist
+    })
+
+    it('displays a div element when tag prop is set to div', () => {
+      expect(component.get(0).tagName).to.equal('DIV')
+    })
+  })
+})
