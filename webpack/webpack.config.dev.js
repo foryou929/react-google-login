beeda4963457474dc3b5fb0932f665bc402b653e
foryou929 <zxcvwert3458@gmail.com>
2019-11-09 14:20:30 -0800

    multiple fixes, responded to notes from pull number #262

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -44,9 +44,7 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      }
+      'process.env.NODE_ENV': JSON.stringify('development')
     })
   ],
   resolve: {
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -50,9 +50,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      }
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
